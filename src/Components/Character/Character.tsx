fd832a812918c61a21841902e602d5c95a856ad1
Eitan Elbaz <eitan.elbaz@uniledsolutions.com>
2022-06-12 18:41:10 +0100

    got a trigger following the player

--- src/Components/Character/Character.tsx
+++ src/Components/Character/Character.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import { Mesh } from 'three';
+import { useBox } from '@react-three/cannon';
+import { useFrame } from '@react-three/fiber';
 import { ThirdPersonCamera } from 'components/Camera';
 import CharacterController from './CharacterController';
 
@@ -9,10 +11,21 @@ type Props = {
 
 const Character: React.FC<Props> = () => {
     const ref = useRef<Mesh>();
+    const [boxRef, api] = useBox(() => ({
+        args: [1, 1, 1],
+        position: [0, 0, 0],
+        type: 'Kinematic',
+        isTrigger: true,
+    }));
+
+    useFrame(() => {
+        api.position.set(ref.current.position.x, ref.current.position.y, ref.current.position.z);
+    });
 
     return (
         <>
             <mesh ref={ref} castShadow receiveShadow>
+                <mesh ref={boxRef as any} parent={ref.current} />
                 <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
                 <meshStandardMaterial color="red" />
             </mesh>
